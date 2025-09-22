02ff576857df40315010330aa8511bcb4a7ea770
Alex72005 <asm0125@alu.medac.es>
2025-09-22 11:53:21 +0200

    servidor y cliente

---
+++ Servidor/tarea1.php
@@ -0,0 +1,135 @@
+<?php
+    /*Ejercicio 1*/
+    $varTexto = "hola";
+    $varEntero = 1234; 
+    $varBoolean = True;
+    $varFloat = 1.2;
+    $varArray = array("1","2");
+
+    echo "Variable de tipo cadena de texto: " . $varTexto . "<br/>";
+    echo "Variable de tipo entero: " . $varEntero . "<br/>";
+    echo "Variable de tipo boolean: " . $varBoolean . "<br/>";
+    echo "Variable de tipo float: " . $varFloat . "<br/>";
+    echo "Variable de tipo compuesto: ";
+        print_r($varArray);
+
+    /*Ejercicio 2*/
+    $nombre = "Alejandro Santos Merina";
+    
+    echo "<br/>"."<br/>" . 'Mostrar variable nombre $nombre';
+    echo "<br/>" . "Mostrar variable nombre $nombre";
+    //Con las comillas simples a un String no puedes concatenarle una variable. 
+
+    /*Ejercicio 3*/
+    echo "<br/>"."<br/>" . gettype($varTexto);
+    echo "<br/>". gettype($varEntero);
+    echo "<br/>". gettype($varBoolean);
+    echo "<br/>". gettype($varFloat);
+    echo "<br/>". gettype($varArray) . "<br/>" . "<br/>";
+
+    $variables = [$varTexto, $varEntero, $varBoolean, $varFloat, $varArray];
+
+    foreach($variables as $valor){
+        if (is_string($valor)){
+            echo "verdadero" . "<br/>";
+        } else{
+            echo "falso" . "<br/>";
+        }
+    }
+
+    $varTexto = "hola";
+    $varEntero = 1234; 
+    $varBoolean = 1;
+    $varFloat = 1.2;
+
+    $variables2 = [$varTexto, $varEntero, $varBoolean, $varFloat, $varArray];
+
+    foreach($variables2 as $valor2){
+        $valor2 = is_string($valor2);
+    }
+
+    // foreach($variables2 as $valor2){
+    //     if (is_string($valor2)){
+    //         echo "verdadero" . "<br/>";
+    //     } else{
+    //         echo "falso" . "<br/>";
+    //     }
+    // }
+
+    /*Ejercicio 4*/
+    //Quítale las tildes a la cadena “Álvaro ha estudiado Ingeniería electrónica en la Universidad de Córdoba”
+    $cadena = "<br/>" . "alvaro ha estudiado ingeniería electrónica en la universidad de córdoba";
+    $buscar = array("á","é","í","ó","ú");
+    $reemplazar = array("a","e","i","o","u");
+
+    $caden= str_replace($buscar,$reemplazar,$cadena);
+
+    echo $caden;
+    //¿Cuántos caracteres tiene la cadena de texto?
+    echo "<br/>" . strlen($cadena);
+    //Convierte la cadena original a mayúsculas.
+    $cad = mb_strtoupper($cadena);
+    echo $cad;
+    //Quita las tildes y los espacios a la cadena de texto “74635498 B Rubén González Díaz”
+    $cadena2 = "74635498 B Rubén González Díaz";
+    $buscar2 = array("á","é","í","ó","ú"," ");
+    $reemplazar2 = array("a","e","i","o","u","");
+
+    $caden2= str_replace($buscar2,$reemplazar2,$cadena2);
+    
+    echo "<br/>" . $caden2;
+?>
+<!-- Ejercicio 5  -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <table style="border: 2px black;">
+        <tr>
+            <td>Person</td>
+            <td>Age</td>
+        </tr>
+        <?php
+            $array = ["Chris" => 38, "Dennis" => 45, "Sarah" => 29, "Karen" => 47];
+            foreach($array as $key => $value){
+                echo "<tr>";
+                echo "<td> $key </td>";
+                echo "<td> $array[$key] </td>";
+                echo "</tr>";
+            }
+        ?>
+    </table> 
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<br>
+<body>
+   <table style="border: 2px black;">
+        <tr>
+            <td>Producto</td>
+            <td>Cantidad</td>
+            <td>Precio</td>
+        </tr>
+        <?php
+            $array = ["Destornillador" => 1, "Llave Inglesa" => 2, "Martillo" => 1];
+            foreach($array as $key => $value){
+                echo "<tr>";
+                echo "<td> $key </td>";
+                echo "<td> $array[$key] </td>";
+                echo "</tr>";
+            }
+        ?>
+    </table> 
+</body>
+</html>
