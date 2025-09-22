fc25ccc3000f7382bb90b644118639a14ede3612
Alex72005 <alexsantosmerina@gmail.com>
2025-09-22 13:09:59 +0200

    tarea 1 servidor

--- Servidor/tarea1.php
+++ Servidor/tarea1.php
@@ -44,17 +44,17 @@
 
     $variables2 = [$varTexto, $varEntero, $varBoolean, $varFloat, $varArray];
 
-    foreach($variables2 as $valor2){
-        $valor2 = is_string($valor2);
+    foreach($variables2 as $key => $valor2){
+        $variables2[$key] = (string)$valor2;
     }
 
-    // foreach($variables2 as $valor2){
-    //     if (is_string($valor2)){
-    //         echo "verdadero" . "<br/>";
-    //     } else{
-    //         echo "falso" . "<br/>";
-    //     }
-    // }
+    foreach($variables2 as $valor2){
+        if (is_string($valor2)){
+            echo "verdadero" . "<br/>";
+        } else{
+            echo "falso" . "<br/>";
+        }
+    }
 
     /*Ejercicio 4*/
     //Quítale las tildes a la cadena “Álvaro ha estudiado Ingeniería electrónica en la Universidad de Córdoba”
@@ -79,6 +79,7 @@
     
     echo "<br/>" . $caden2;
 ?>
+
 <!-- Ejercicio 5  -->
 <!DOCTYPE html>
 <html lang="es">
@@ -86,9 +87,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table{
+            border: 2px solid black;
+            border-collapse: collapse;
+        }
+
+        tr, td{
+            border: 2px solid black;
+        }
+
+    </style>
 </head>
 <body>
-   <table style="border: 2px black;">
+   <table>
         <tr>
             <td>Person</td>
             <td>Age</td>
@@ -102,6 +114,7 @@
                 echo "</tr>";
             }
         ?>
+
     </table> 
 </body>
 </html>
@@ -112,24 +125,106 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table{
+            border: 2px solid black;
+            border-collapse: collapse;
+        }
+
+        tr, td{
+            border: 2px solid black;
+        }
+
+    </style>
 </head>
 <br>
 <body>
-   <table style="border: 2px black;">
+   <table>
         <tr>
             <td>Producto</td>
             <td>Cantidad</td>
             <td>Precio</td>
         </tr>
         <?php
-            $array = ["Destornillador" => 1, "Llave Inglesa" => 2, "Martillo" => 1];
+            $total = 0;
+            $array = ["Destornillador" => 1, "Llave Inglesa" => 2, "Martillo" => 1];  //producto => cantidad
+            $array2 = ["Destornillador" => 3, "Llave Inglesa" => 5, "Martillo" => 7]; //producto => precio
+
             foreach($array as $key => $value){
+                $total += $value * $array2[$key];
                 echo "<tr>";
                 echo "<td> $key </td>";
-                echo "<td> $array[$key] </td>";
+                echo "<td> $value </td>";
+                echo "<td> $array2[$key] </td>";
                 echo "</tr>";
             }
+
+            echo "<tr>";
+            echo "<td colspan = '2'>Total </td>";
+            echo "<td>$total</td>";
+            echo "</tr>";
+
         ?>
     </table> 
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table{
+            border: 2px solid black;
+            border-collapse: collapse;
+        }
+
+        tr, td{
+            border: 2px solid black;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>Su pedido</h1>
+
+    <table>
+        <tr>
+            <td><b>Nombre producto</b></td>
+            <td><b>Precio unitario</b></td>
+            <td><b>Unidades</b></td>
+            <td><b>Subtotal</b></td>
+        </tr>
+        <?php
+            $subtotal = 0;
+            $total = 0;
+            $unidades = 0;
+            $array3 = ["Reproductor MP3 (80 GB)" => 192.02, "Fundas de colores" => 2.50, "Reproductor de radio & control remoto" => 12.99];  //Nombre producto => precio unitario
+            $array4 = ["Reproductor MP3 (80 GB)" => 1, "Fundas de colores" => 5, "Reproductor de radio & control remoto" => 1]; //Nombre producto => unidades
+
+            foreach($array3 as $key => $value){
+                $subtotal =  $value * $array4[$key];
+                $total += $subtotal;
+                $unidades += $array4[$key];
+                echo "<tr>";
+                echo "<td> $key </td>";
+                echo "<td> $value </td>";
+                echo "<td> $array4[$key] </td>";
+                echo "<td> $subtotal</td>";
+                echo "</tr>";
+            }
+
+            echo "<tr>";
+            echo "<td>Total </td>";
+            echo "<td>-</td>";
+            echo "<td>$unidades</td>";
+            echo "<td>$total</td>";
+            echo "</tr>";
+
+        ?>
+    </table> 
+
 </body>
 </html>
