d0e633bde87a73352d536911615038681bba2761
Alex72005 <asm0125@alu.medac.es>
2025-09-25 18:32:21 +0200

    1

--- Cliente/Pruebas/Prueba 1/prueba1.js
+++ Cliente/Pruebas/Prueba 1/prueba1.js
@@ -1,38 +1,38 @@
-//Ejercicio 1: cara o cruz. 
-// let opcion = "";
-// let racha = 0;
-// let cantidadrondasseguidas = 0;
-// let opcionrandom = 0;
-// let opcionsalido = "";
+// Ejercicio 1: cara o cruz. 
+let opcion = "";
+let racha = 0;
+let cantidadrondasseguidas = 0;
+let opcionrandom = 0;
+let opcionsalido = "";
 
-// const getRandomInt = max => Math.floor(Math.random() * max);
+const getRandomInt = max => Math.floor(Math.random() * max);
 
-// do {
-//     opcion = prompt("Introduzca CARA o CRUZ o SALIR: ");
-//     opcionrandom = getRandomInt(2);
+do {
+    opcion = prompt("Introduzca CARA o CRUZ o SALIR: ");
+    opcionrandom = getRandomInt(2);
 
-//     if (opcion != "SALIR") {
-//         if (opcionrandom == 0) {
-//             alert("La moneda ha salido CARA");
-//             opcionsalido = "CARA";
-//         } else if (opcionrandom == 1) {
-//             alert("La moneda ha salido CRUZ");
-//             opcionsalido = "CRUZ";
-//         }
+    if (opcion != "SALIR") {
+        if (opcionrandom == 0) {
+            alert("La moneda ha salido CARA");
+            opcionsalido = "CARA";
+        } else if (opcionrandom == 1) {
+            alert("La moneda ha salido CRUZ");
+            opcionsalido = "CRUZ";
+        }
 
-//         if (opcion == opcionsalido) {
-//             racha++;
-//             alert("¡Has acertado! Racha= " + racha + " acierto");
-//         } else {
-//             cantidadrondasseguidas = racha;
-//             racha = 0;
-//             alert("¡Has fallado!")
-//         }
-//     }
+        if (opcion == opcionsalido) {
+            racha++;
+            alert("¡Has acertado! Racha= " + racha + " acierto");
+        } else {
+            cantidadrondasseguidas = racha;
+            racha = 0;
+            alert("¡Has fallado!")
+        }
+    }
 
-// } while (opcion != "SALIR");
+} while (opcion != "SALIR");
 
-// alert("¡Hasta pronto! Cantidad de rondas seguidas de aciertos: " + cantidadrondasseguidas);
+alert("¡Hasta pronto! Cantidad de rondas seguidas de aciertos: " + cantidadrondasseguidas);
 
 
 //Ejercicio 2. 
