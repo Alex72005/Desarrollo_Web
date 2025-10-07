840f4e611516c01fb5272b805c50815116c02e57
Alex72005 <alexsantosmerina@gmail.com>
2025-10-07 13:20:27 +0200

    ss

--- Cliente/Pruebas/Prueba 1 DOM/script copy.js
+++ Cliente/Pruebas/Prueba 1 DOM/script copy.js
@@ -3,7 +3,8 @@ const cartas = ["1.jpg", "2.jpg", "1.jpg", "4.jpg", "3.jpg", "2.jpg", "4.jpg", "
 const tablero = document.querySelector("#tablero");
 let parejasEncontradas = 0;
 let seleccionadas = [];
-let intentos = 5;
+let intentos = 10;
+let juegoActivo = true;
 
 const baseUrl = "img/";
 const cartaOculta = baseUrl + "atras.png";
@@ -26,43 +27,47 @@ setTimeout(() => {
 
 imagenes.forEach(img => {
     img.addEventListener("click", () => {
-        const indice = parseInt(img.dataset.posicion);
+        if (juegoActivo) {
+            const indice = parseInt(img.dataset.posicion);
 
-        // Solo si no está ya seleccionada y no hay dos seleccionadas
-        if (oculto[indice] == "X" && seleccionadas.length < 2) {
-            img.src = baseUrl + cartas[indice]; 
-            oculto[indice] = cartas[indice]; 
-            seleccionadas.push(indice);
+            // Solo si no está ya seleccionada y no hay dos seleccionadas
+            if (oculto[indice] == "X" && seleccionadas.length < 2) {
+                img.src = baseUrl + cartas[indice];
+                oculto[indice] = cartas[indice];
+                seleccionadas.push(indice);
 
-            // Cuando hay dos cartas seleccionadas
-            if (seleccionadas.length == 2) {
-                intentos--;
-                const carta1 = seleccionadas[0];
-                const carta2 = seleccionadas[1];
+                // Cuando hay dos cartas seleccionadas
+                if (seleccionadas.length == 2) {
+                    intentos--;
+                    const carta1 = seleccionadas[0];
+                    const carta2 = seleccionadas[1];
 
-                // Esperamos un poco para que se vea la segunda carta
-                setTimeout(() => {
-                    if (cartas[carta1] == cartas[carta2]) {
-                        alert("¡Has encontrado una pareja!");
-                        parejasEncontradas++;
-                    } else {
-                        alert("No son iguales");
-                        // Tapar las cartas
-                        imagenes[carta1].src = cartaOculta;
-                        imagenes[carta2].src = cartaOculta;
-                        oculto[carta1] = "X";
-                        oculto[carta2] = "X";
-                    }
+                    // Esperamos un poco para que se vea la segunda carta
+                    setTimeout(() => {
+                        if (cartas[carta1] == cartas[carta2]) {
+                            alert("¡Has encontrado una pareja!");
+                            parejasEncontradas++;
+                        } else {
+                            alert("No son iguales");
+                            // Tapar las cartas
+                            imagenes[carta1].src = cartaOculta;
+                            imagenes[carta2].src = cartaOculta;
+                            oculto[carta1] = "X";
+                            oculto[carta2] = "X";
+                        }
 
-                    seleccionadas = [];
+                        seleccionadas = [];
 
-                    // Comprobar si se encontraron todas las parejas
-                    if (parejasEncontradas == cartas.length / 2) {
-                        alert("¡Has encontrado todas las parejas. Enhorabuena!");
-                    } else if (intentos === 0) {
-                        alert("Te quedaste sin intentos");
-                    }
-                }, 500); // 0.5 segundos para que se vea la segunda carta
+                        // Comprobar si se encontraron todas las parejas
+                        if (parejasEncontradas == cartas.length / 2) {
+                            alert("¡Has encontrado todas las parejas. Enhorabuena!");
+                            juegoActivo = false;
+                        } else if (intentos === 0) {
+                            alert("Te quedaste sin intentos");
+                            juegoActivo = false;
+                        }
+                    }, 500); // 0.5 segundos para que se vea la segunda carta
+                }
             }
         }
     });
