1a4cd1d6d8c349c50348ca9373460a41c71a175c
Alex72005 <asm0125@alu.medac.es>
2025-10-09 19:07:29 +0200

    cara-cruz

---
+++ Cliente/Clase/DOM/cara_cruz/script.js
@@ -0,0 +1,36 @@
+const imagenes = document.querySelectorAll("img")
+const resultado = document.querySelector("#resultado")
+const getRandomInt = max => Math.floor(Math.random() * max);
+let racha = 0;
+let rachaMax = 0;
+
+imagenes.forEach(img => {
+    img.addEventListener("click", () => {
+        let aleatoria = getRandomInt(2);
+        let resultadoJuego = "";
+        let nuevaImg = document.createElement("img")
+
+        if (aleatoria == 0) {
+            resultadoJuego = "cara"
+            nuevaImg.src = "img/cara.png"
+        } else if (aleatoria == 1) {
+            resultadoJuego = "cruz"
+            nuevaImg.src = "img/cruz.png"
+        }
+
+        resultado.innerHTML = "";
+        resultado.appendChild(nuevaImg);
+
+        if (img.id == resultadoJuego) {
+            racha += 1;
+            if (racha > rachaMax) {
+                rachaMax = racha;
+            }
+        } else {
+            racha = 0;
+        }
+
+        document.querySelector("#racha").textContent = "Racha actual: " + racha;
+        document.querySelector("#rachaMax").textContent = "Racha máxima: " + rachaMax;
+    })
+})
